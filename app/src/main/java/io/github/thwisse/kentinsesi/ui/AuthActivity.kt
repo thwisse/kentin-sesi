8d51b283f349c7d8d9f28d9c0cc059f9ffd0b3c9
Oğuzhan Tutucu <thwisse@tutanota.com>
2025-10-21 17:36:05 +0300

    uygulama iconu ayarlandi

--- app/src/main/java/io/github/thwisse/kentinsesi/ui/AuthActivity.kt
+++ app/src/main/java/io/github/thwisse/kentinsesi/ui/AuthActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import io.github.thwisse.kentinsesi.R
 import io.github.thwisse.kentinsesi.databinding.ActivityAuthBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
